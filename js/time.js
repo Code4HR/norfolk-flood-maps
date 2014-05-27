4f86bfbadd078e983ff782871e0e101b43beb9df
stanzheng <dev.stanzheng@gmail.com>
2014-05-26 22:49:08 -0400

    time correctly adding

--- js/time.js
+++ js/time.js
@@ -1,11 +1,14 @@
- var d = new Date();
-    var curr_date = d.getDate();
-    var curr_month = d.getMonth() + 1; //Months are zero based
-    var curr_year = d.getFullYear();
-    var today = curr_year + "" + curr_month +""+ curr_date;
-
 function getTide(){
-  var url = "http://www.corsproxy.com/tidesandcurrents.noaa.gov/api/datagetter?product=water_level&application=NOS.COOPS.TAC.WL&begin_date=20140525&end_date=20140525&datum=MLLW&station=8638610&time_zone=GMT&units=english&format=json";
+var d = new Date();
+   var curr_date = d.getDate();
+   var curr_month = d.getMonth() + 1; //Months are zero based
+   if(curr_month<10){
+    curr_month ="0"+curr_month;
+   }
+   var curr_year = d.getFullYear();
+   var today = curr_year + "" + curr_month +""+ curr_date;
+console.log(today);
+  var url = "http://www.corsproxy.com/tidesandcurrents.noaa.gov/api/datagetter?product=water_level&application=NOS.COOPS.TAC.WL&begin_date="+today+"&end_date="+today+"&datum=MLLW&station=8638610&time_zone=GMT&units=english&format=json";
 $.getJSON( url, function( data ) {
   // var items = [];
   // $.each( data, function( key, val ) {
