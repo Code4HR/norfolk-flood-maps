298729abfe74aa65d40b0f51194cbd0beb3897ff
stanzheng <dev.stanzheng@gmail.com>
2014-05-26 22:29:10 -0400

    getting today

--- js/time.js
+++ js/time.js
@@ -1,4 +1,9 @@
-console.log(Date.now());
+ var d = new Date();
+    var curr_date = d.getDate();
+    var curr_month = d.getMonth() + 1; //Months are zero based
+    var curr_year = d.getFullYear();
+    var today = curr_year + "" + curr_month +""+ curr_date;
+
 function getTide(){
   var url = "http://www.corsproxy.com/tidesandcurrents.noaa.gov/api/datagetter?product=water_level&application=NOS.COOPS.TAC.WL&begin_date=20140525&end_date=20140525&datum=MLLW&station=8638610&time_zone=GMT&units=english&format=json";
 $.getJSON( url, function( data ) {
