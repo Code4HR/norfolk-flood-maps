500774860f62061c07b04a6cb40841fe890e172a
stanzheng <dev.stanzheng@gmail.com>
2014-05-26 22:18:45 -0400

    trying to get time now for current day

--- js/time.js
+++ js/time.js
@@ -1,13 +1,18 @@
-var url = "http://tidesandcurrents.noaa.gov/api/datagetter?product=water_level&application=NOS.COOPS.TAC.WL&begin_date=20140525&end_date=20140526&datum=MLLW&station=8638610&time_zone=GMT&units=english&format=json";
+console.log(Date.now());
+function getTide(){
+  var url = "http://www.corsproxy.com/tidesandcurrents.noaa.gov/api/datagetter?product=water_level&application=NOS.COOPS.TAC.WL&begin_date=20140525&end_date=20140525&datum=MLLW&station=8638610&time_zone=GMT&units=english&format=json";
 $.getJSON( url, function( data ) {
-  var items = [];
-  $.each( data, function( key, val ) {
-    items.push( "<li id='" + key + "'>" + val + "</li>" );
+  // var items = [];
+  // $.each( data, function( key, val ) {
+  //   items.push( "<li id='" + key + "'>" + val + "</li>" );
+  // });
+  //
+  // $( "<ul/>", {
+  //   "class": "my-new-list",
+  //   html: items.join( "" )
+  // }).appendTo( "body" );
+  console.log(data);
+  time=data;
   });
-
-  $( "<ul/>", {
-    "class": "my-new-list",
-    html: items.join( "" )
-  }).appendTo( "body" );
-});
-console.log("hi");
+}
+var time =getTide();
