52f21f923de853e394d1694aa2efead557405bde
Zach Young <young.zach@gmail.com>
2014-05-31 13:52:08 -0400

    ran jsbeautifier on code

--- js/main.js
+++ js/main.js
@@ -1,53 +1,52 @@
-
-function getTide(){
-var d = new Date();
-   var curr_date = d.getDate();
-   var curr_month = d.getMonth() + 1; //Months are zero based
-   if(curr_month<10){
-    curr_month ="0"+curr_month;
-   }
-   var curr_year = d.getFullYear();
-   var today = curr_year + "" + curr_month +""+ curr_date;
-console.log(today);
-  var url = "http://www.corsproxy.com/tidesandcurrents.noaa.gov/api/datagetter?product=water_level&application=NOS.COOPS.TAC.WL&begin_date="+today+"&end_date="+today+"&datum=MLLW&station=8638610&time_zone=GMT&units=english&format=json";
-$.getJSON( url, function( data ) {
-  var time = [];
-  _.each(data.data, function(data){
-    //round the data
-    time.push(roundHalf(data.v));
-    //push it
-    //add as slider lookup
+function getTide() {
+    var d = new Date();
+    var curr_date = d.getDate();
+    var curr_month = d.getMonth() + 1; //Months are zero based
+    if (curr_month < 10) {
+        curr_month = "0" + curr_month;
+    }
+    var curr_year = d.getFullYear();
+    var today = curr_year + "" + curr_month + "" + curr_date;
+    console.log(today);
+    var url = "http://www.corsproxy.com/tidesandcurrents.noaa.gov/api/datagetter?product=water_level&application=NOS.COOPS.TAC.WL&begin_date=" + today + "&end_date=" + today + "&datum=MLLW&station=8638610&time_zone=GMT&units=english&format=json";
+    $.getJSON(url, function (data) {
+        var time = [];
+        _.each(data.data, function (data) {
+            //round the data
+            time.push(roundHalf(data.v));
+            //push it
+            //add as slider lookup
+        });
+        console.log(time);
     });
-  console.log(time);
-  });
 }
-var time =getTide();
+var time = getTide();
 
 
 function roundHalf(num) {
-    num = Math.round(num*2)/2;
+    num = Math.round(num * 2) / 2;
     return num;
 }
 
-function updateTime(value){
-  var d = new Date();
-  $("otherSlide").val(d.getMinutes()/6);
-  var minutes = value;
-  var hours = Math.floor(minutes/60);
-  $("#time").text(Date.now());
-  // console.log(minutes);
-  // console.log(hours);
-  // console.log(hours + ""+minutes);
+function updateTime(value) {
+    var d = new Date();
+    $("otherSlide").val(d.getMinutes() / 6);
+    var minutes = value;
+    var hours = Math.floor(minutes / 60);
+    $("#time").text(Date.now());
+    // console.log(minutes);
+    // console.log(hours);
+    // console.log(hours + ""+minutes);
 }
 
 function showControls() {
-  var t = document.getElementById("title");
-  var c = document.getElementById("controlContainer");
-  if (c .className == "control-container") {
-    t.className = "title expand"
-    c.className = "control-container hide";
-  } else {
-    t.className = "title contract"
-    c.className = "control-container";
-  }
+    var t = document.getElementById("title");
+    var c = document.getElementById("controlContainer");
+    if (c.className == "control-container") {
+        t.className = "title expand"
+        c.className = "control-container hide";
+    } else {
+        t.className = "title contract"
+        c.className = "control-container";
+    }
 }
