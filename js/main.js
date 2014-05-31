e6114fc86e0b94453de80bb2f476cf86e6922f65
Zach Young <young.zach@gmail.com>
2014-05-31 13:47:22 -0400

    moved example css into main.css and moved time.js > main.js for future additions of more js

--- js/main.js
+++ js/main.js
@@ -39,3 +39,15 @@ function updateTime(value){
   // console.log(hours);
   // console.log(hours + ""+minutes);
 }
+
+function showControls() {
+  var t = document.getElementById("title");
+  var c = document.getElementById("controlContainer");
+  if (c .className == "control-container") {
+    t.className = "title expand"
+    c.className = "control-container hide";
+  } else {
+    t.className = "title contract"
+    c.className = "control-container";
+  }
+}
